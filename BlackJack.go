a6b05ddcad6f204cca69d2c4fab20d601746d3ea
Vinicius <vinilm2003@gmail.com>
2020-07-18 21:22:15 -0300

    Adição dos naipes das cartas

--- BlackJack.go
+++ BlackJack.go
@@ -28,15 +28,17 @@ func main() {
 
 type Carta struct {
   Nome string
+  Naipe string
   Pontos int32
 }
 
 func jogo(cartas [13]Carta){
   var resposta string
 
+  naipe := entregarNaipe(cartas)
   carta := entregarCarta(cartas)
   pontos := carta.Pontos
-  fmt.Printf("A carta sorteada é %s\nSua pontuação atual é %d\n", carta.Nome, pontos)
+  fmt.Printf("A carta sorteada é %s %s\nSua pontuação atual é %d\n", carta.Nome, naipe.Naipe, pontos)
 
   for pontos <= 21 {
     fmt.Printf("Você gostaria de mais uma carta? \n")
@@ -44,16 +46,17 @@ func jogo(cartas [13]Carta){
     respVerificada := simOuNao(resposta)
 
     if respVerificada{
+      naipe = entregarNaipe(cartas)
       carta = entregarCarta(cartas)
       pontos = pontos + carta.Pontos
       if pontos > 21{
-        fmt.Printf("A carta sorteada é %s sua pontuação foi %d\n      Game Over\n", carta.Nome, pontos)
+        fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n      Game Over\n", carta.Nome, naipe.Naipe, pontos)
         os.Exit(0)
       } else if pontos == 21{
-        fmt.Printf("A carta sorteada é %s sua pontuação foi %d\nParabéns!\n      Game Over\n", carta.Nome, pontos)
+        fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\nParabéns!\n      Game Over\n", carta.Nome, naipe.Naipe, pontos)
         os.Exit(0)
       }
-      fmt.Printf("A carta sorteada é %s\nSua pontuação atual é %d\n", carta.Nome, pontos)
+      fmt.Printf("A carta sorteada é %s %s\nSua pontuação atual é %d\n", carta.Nome, naipe.Naipe, pontos)
     } else{
       fmt.Printf("Sua pontuação foi %d\nObrigado por jogar!\n", pontos)
       os.Exit(0)
@@ -62,15 +65,19 @@ func jogo(cartas [13]Carta){
 }
 
 func gerarCartas ()(cartas [13]Carta){
+  cartas[0].Naipe = "de Paus"
   cartas[0].Nome = "Dois"
   cartas[0].Pontos = 2
 
+  cartas[1].Naipe = "de Ouros"
   cartas[1].Nome = "Três"
   cartas[1].Pontos = 3
 
+  cartas[2].Naipe = "de Copas"
   cartas[2].Nome = "Quatro"
   cartas[2].Pontos = 4
 
+  cartas[3].Naipe = "de Espadas"
   cartas[3].Nome = "Cinco"
   cartas[3].Pontos = 5
 
@@ -116,17 +123,31 @@ func simOuNao(resp string)(sim bool){
   return
 }
 
-func gerarRandom()(random int32){
+func gerarCartaRandom()(random int32){
+  return gerarRandom(13)
+}
+
+func entregarCarta (cartas [13]Carta)(carta Carta){
+  num := gerarCartaRandom()
+  carta = cartas[num]
+  return
+}
+
+func gerarNaipeRandom()(random int32){
+  return gerarRandom(4)
+}
+
+func gerarRandom(limite int32)(random int32){
   tempo := time.Now()
   nanosecond := tempo.Nanosecond()
   rand.Seed(int64(nanosecond))
-  random = rand.Int31n(13)
+  random = rand.Int31n(limite)
 
   return
 }
 
-func entregarCarta (cartas [13]Carta)(carta Carta){
-  num := gerarRandom()
-  carta = cartas[num]
+func entregarNaipe (cartas [13]Carta)(naipeCarta Carta){
+  num := gerarNaipeRandom()
+  naipeCarta = cartas[num]
   return
 }

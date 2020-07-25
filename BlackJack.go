9d4edaf89c270ed434913bf7f4849a003a397bcc
Vinicius <vinilm2003@gmail.com>
2020-07-24 22:08:39 -0300

    Atualização na função de gerar carta

--- BlackJack.go
+++ BlackJack.go
@@ -11,15 +11,13 @@ import (
 func main() {
   var resposta string
   var jogar bool
-  var cartas [13]Carta
 
-  cartas = gerarCartas()
   fmt.Printf("Bem vindo ao BlackJack \nGostaria de iniciar uma partida? \n")
   fmt.Scanln(&resposta)
   jogar = simOuNao(resposta)
 
   if jogar {
-    jogo(cartas)
+    jogo()
   } else {
     fmt.Printf("Até a próxima!\n")
     os.Exit(0)
@@ -32,110 +30,59 @@ type Carta struct {
   Pontos int32
 }
 
-func jogo(cartas [13]Carta){
+func jogo(){
   var resposta string
 
-  naipePC, cartaPC := cartasDealer(cartas)
+  cartaPC := gerarCarta()
   pontosPC := cartaPC.Pontos
-  fmt.Printf("A carta do dealer é %s %s\n E sua pontuação atual é %d\n", cartaPC.Nome, naipePC.Naipe, pontosPC)
+  fmt.Printf("A carta do dealer é %s %s\n E sua pontuação atual é %d\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
 
-  naipe, carta := cartasJogador(cartas)
-  pontos := carta.Pontos
-  fmt.Printf("\nA sua carta sorteada é %s %s\nSua pontuação atual é %d\n", carta.Nome, naipe.Naipe, pontos)
+  cartaJ := gerarCarta()
+  pontosJ := cartaJ.Pontos
+  fmt.Printf("\nA sua carta sorteada é %s %s\nSua pontuação atual é %d\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
 
-  for pontos <= 21 {
+  for pontosJ <= 21 {
     fmt.Printf("Você gostaria de mais uma carta? \n")
     fmt.Scanln(&resposta)
     respVerificada := simOuNao(resposta)
 
     if respVerificada{
-      naipePC, cartaPC = cartasDealer(cartas)
+      cartaPC = gerarCarta()
       pontosPC = pontosPC + cartaPC.Pontos
 
-      naipe, carta = cartasJogador(cartas)
-      pontos = pontos + carta.Pontos
+      cartaJ = gerarCarta()
+      pontosJ = pontosJ + cartaJ.Pontos
 
-      verificacaoJogador(naipe, carta, naipePC, cartaPC, pontos, pontosPC)
+      verificacaoJogador(cartaJ, cartaPC, pontosJ, pontosPC)
     } else{
       fmt.Printf("O dealer terminou com %d pontos\n", pontosPC)
-      fmt.Printf("Sua pontuação foi %d\nObrigado por jogar!\n", pontos)
+      fmt.Printf("Sua pontuação foi %d\nObrigado por jogar!\n", pontosJ)
       os.Exit(0)
     }
   }
 }
 
-func gerarCartas ()(cartas [13]Carta){
-  cartas[0].Naipe = "de Paus"
-  cartas[0].Nome = "Dois"
-  cartas[0].Pontos = 2
-
-  cartas[1].Naipe = "de Ouros"
-  cartas[1].Nome = "Três"
-  cartas[1].Pontos = 3
-
-  cartas[2].Naipe = "de Copas"
-  cartas[2].Nome = "Quatro"
-  cartas[2].Pontos = 4
-
-  cartas[3].Naipe = "de Espadas"
-  cartas[3].Nome = "Cinco"
-  cartas[3].Pontos = 5
-
-  cartas[4].Nome = "Seis"
-  cartas[4].Pontos = 6
-
-  cartas[5].Nome = "Sete"
-  cartas[5].Pontos = 7
-
-  cartas[6].Nome = "Oito"
-  cartas[6].Pontos = 8
-
-  cartas[7].Nome = "Nove"
-  cartas[7].Pontos = 9
-
-  cartas[8].Nome = "Dez"
-  cartas[8].Pontos = 10
-
-  cartas[9].Nome = "Valete"
-  cartas[9].Pontos = 10
-
-  cartas[10].Nome = "Dama"
-  cartas[10].Pontos = 10
-
-  cartas[11].Nome = "Rei"
-  cartas[11].Pontos = 10
-
-  cartas[12].Nome = "As"
-  cartas[12].Pontos = 1
-
-  return
-}
-
-func verificacaoJogador(naipe Carta, carta Carta, naipePC Carta, cartaPC Carta, pontos int32, pontosPC int32){
-  if pontos > 21 && pontosPC < 21{
-    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n      Game Over\n", carta.Nome, naipe.Naipe, pontos)
-    fmt.Printf("A carta do dealer é %s %s, ele venceu com %d pontos!\n      Game Over\n", cartaPC.Nome, naipePC.Naipe, pontosPC)
+func verificacaoJogador(cartaJ Carta, cartaPC Carta, pontosJ int32, pontosPC int32){
+  if pontosJ > 21 && pontosPC < 21{
+    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n      Game Over\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
+    fmt.Printf("A carta do dealer é %s %s, ele venceu com %d pontos!\n      Game Over\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
     os.Exit(0)
-  } else if pontos == 21{
-    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n", carta.Nome, naipe.Naipe, pontos)
-    fmt.Printf("A carta do dealer é %s %s, ele perdeu com %d pontos\nParabéns!\n      Game Over\n", cartaPC.Nome, naipePC.Naipe, pontosPC)
+  } else if pontosJ == 21{
+    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
+    fmt.Printf("A carta do dealer é %s %s, ele perdeu com %d pontos\nParabéns!\n      Game Over\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
+    os.Exit(0)
+  } else if pontosPC == 21{
+    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n      Game Over\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
+    fmt.Printf("A carta do dealer é %s %s, ele venceu com %d pontos!\n      Game Over\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
+    os.Exit(0)
+  } else if pontosPC > 21 && pontosJ < 21{
+    fmt.Printf("A carta sorteada é %s %s sua pontuação foi %d\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
+    fmt.Printf("A carta do dealer é %s %s, ele perdeu com %d pontos\nParabéns!\n      Game Over\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
     os.Exit(0)
   } else {
-  fmt.Printf("A carta do dealer é %s %s\n E sua pontuação atual é %d\n", cartaPC.Nome, naipePC.Naipe, pontosPC)
-  fmt.Printf("\nA sua carta sorteada é %s %s\nSua pontuação atual é %d\n", carta.Nome, naipe.Naipe, pontos)
+  fmt.Printf("A carta do dealer é %s %s\n E sua pontuação atual é %d\n", cartaPC.Nome, cartaPC.Naipe, pontosPC)
+  fmt.Printf("\nA sua carta sorteada é %s %s\nSua pontuação atual é %d\n", cartaJ.Nome, cartaJ.Naipe, pontosJ)
   }
-  return
-}
-
-func cartasDealer (cartas [13]Carta) (naipe Carta, carta Carta){
-  naipe = entregarNaipe(cartas)
-  carta = entregarCarta(cartas)
-  return
-}
-
-func cartasJogador (cartas [13]Carta) (naipe Carta, carta Carta){
-  naipe = entregarNaipe(cartas)
-  carta = entregarCarta(cartas)
   return
 }
 
@@ -151,31 +98,28 @@ func simOuNao(resp string)(sim bool){
   return
 }
 
-func gerarCartaRandom()(random int32){
-  return gerarRandom(13)
+func gerarCarta()(carta Carta){
+  var nome = [13]string{"Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete", "Rainha", "Rei", "As"}
+  var naipe = [4]string{"Ouros", "Copas", "Espadas", "Paus"}
+  var pontos = [13]int32{2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1}
+
+  num := gerarRandom()
+  numCarta := num % 13
+  numPontos := num % 13
+  numNaipe := num % 4
+
+  carta.Nome = nome[numCarta]
+  carta.Naipe = naipe[numNaipe]
+  carta.Pontos = pontos[numPontos]
+
+return
 }
 
-func entregarCarta (cartas [13]Carta)(carta Carta){
-  num := gerarCartaRandom()
-  carta = cartas[num]
-  return
-}
-
-func gerarNaipeRandom()(random int32){
-  return gerarRandom(4)
-}
-
-func gerarRandom(limite int32)(random int32){
+func gerarRandom()(random int32){
   tempo := time.Now()
   nanosecond := tempo.Nanosecond()
   rand.Seed(int64(nanosecond))
-  random = rand.Int31n(limite)
+  random = rand.Int31n(52)
 
-  return
-}
-
-func entregarNaipe (cartas [13]Carta)(naipeCarta Carta){
-  num := gerarNaipeRandom()
-  naipeCarta = cartas[num]
   return
 }
